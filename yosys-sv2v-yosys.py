cc6ff34fe8ae1c9e0b20cc9993e1ea9d981e8417
Kamil Rakoczy <krakoczy@antmicro.com>
2020-06-17 15:25:13 +0200

    Update yosys, sv2v files and ibex.patch

--- yosys-sv2v-yosys.py
+++ yosys-sv2v-yosys.py
@@ -9,13 +9,15 @@ work_root = 'yosys-sv2v-yosys-build'
 os.makedirs(work_root, exist_ok=True)
 
 srcs = glob.glob("./ibex/build/**/*.sv", recursive=True)
+sram_init = glob.glob("./ibex/**/led.vmem", recursive=True)
+SRAM_INIT_FILE_PATH = f"{os.path.abspath(os.getcwd())}/{sram_init[0]}"
 
 files = [
     {'name': os.path.realpath('./ibex/build/lowrisc_ibex_top_artya7_0.1/src/lowrisc_ibex_top_artya7_0.1/data/pins_artya7.xdc'), 'file_type': 'xdc'},
     {'name': os.path.realpath('./ibex.v'), 'file_type': 'verilogSource'}
 ]
 
-skip_files = ["ibex_icache.sv", "ibex_cs_registers.sv", "ibex_fetch_fifo.sv", "ibex_core.sv", "ibex_pmp.sv", "ibex_alu.sv"]
+skip_files = ["ibex_icache.sv", "ibex_fetch_fifo.sv", "ibex_core.sv", "ibex_pmp.sv"]
 
 sv2v_files = []
 
@@ -35,13 +37,20 @@ for src in srcs:
       files.append({'name': os.path.realpath(src), 'file_type': 'systemVerilogSource'})
 
 
-cmd = ["sv2v", f"--incdir={PRIM_ASSERT_DIR}"]
+cmd = ["sv2v", f"--define=SRAM_INIT_FILE={SRAM_INIT_FILE_PATH}", f"--incdir={PRIM_ASSERT_DIR}"]
 for sv2v_file in sv2v_files:
     cmd.append(sv2v_file)
 with open("ibex.v", "w") as f:
     subprocess.call(cmd, stdout=f)
 
-parameters = {}
+parameters = {
+    'SRAM_INIT_FILE':
+    {
+	     'paramtype': 'vlogdefine',
+	     'datatype': 'str',
+	     'default': SRAM_INIT_FILE_PATH,
+    }
+}
 tool = 'yosys'
 yosys_synth_options = '-iopad -family xc7'
 incdirs = {}
